82e3abbc2d1bbfd1a3b6a03a9d4c2f2019449d11
Luis Martinez <luis.e.martinez@tamu.edu>
2022-10-03 21:32:13 -0500

    jdbcpost SQL Java file

---
+++ jdbcpostgreSQL.java
@@ -0,0 +1,93 @@
+import java.sql.*;
+import java.util.Scanner;
+import java.io.*;
+
+/*
+CSCE 331
+9-28-2022 Lab
+ */
+public class jdbcpostgreSQL {
+
+  //Commands to run this script
+  //This will compile all java files in this directory
+  //javac *.java
+  //This command tells the file where to find the postgres jar which it needs to execute postgres commands, then executes the code
+
+  /* DON"T COPY PASTE WRITE THE COMMANDS IN YOUR TERMINAL MANUALLY*/
+
+  //Windows: java -cp ".;postgresql-42.2.8.jar" jdbcpostgreSQL
+  //Mac/Linux: java -cp ".:postgresql-42.2.8.jar" jdbcpostgreSQL
+
+  //MAKE SURE YOU ARE ON VPN or TAMU WIFI TO ACCESS DATABASE
+  public static void main(String args[]) {
+
+    //Building the connection with your credentials
+    Connection conn = null;
+    String teamNumber = "63"; // Your team number
+    String sectionNumber = "905"; // Your section number
+    String dbName = "csce331_" + sectionNumber + "_" + teamNumber;
+    String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+    dbSetup myCredentials = new dbSetup(); 
+
+    //Connecting to the database
+    try {
+        conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+     } catch (Exception e) {
+        e.printStackTrace();
+        System.err.println(e.getClass().getName()+": "+e.getMessage());
+        System.exit(0);
+     }
+
+     System.out.println("Opened database successfully");
+
+     try{
+       //create a statement object
+       Statement stmt = conn.createStatement();
+
+       //Running a query
+       //TODO: update the sql command here
+       File order_csv = new File("order.csv");
+       Scanner myReader = new Scanner(order_csv);
+       String sqlStatement = "";
+       while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        String[] items = data.split(",");
+        //System.out.println(items[3]);
+        sqlStatement = "INSERT INTO \"order\" (OrderID, FoodID, Quantity, OrderDate, Amount) VALUES(" + items[0] + "," + items[1] + "," + 
+                              items[2] + ",'" + items[3] + "'," + items[4] + ");";
+        stmt.executeUpdate(sqlStatement);
+        
+       }
+       //System.out.println(sqlStatement.length());
+       
+
+       //send statement to DBMS
+       //This executeQuery command is useful for data retrieval
+       //ResultSet result = stmt.executeQuery(sqlStatement);
+       //OR
+       //This executeUpdate command is useful for updating data
+       //int result = stmt.executeUpdate(sqlStatement);
+
+       //OUTPUT
+       //You will need to output the results differently depending on which function you use
+       System.out.println("--------------------Query Results--------------------");
+       //while (result.next()) {
+       //System.out.println(result.getString("column_name"));
+       //}
+       //OR
+       //System.out.println(result);
+   } catch (Exception e){
+       e.printStackTrace();
+       System.err.println(e.getClass().getName()+": "+e.getMessage());
+       System.exit(0);
+   }
+
+    //closing the connection
+    try {
+      conn.close();
+      System.out.println("Connection Closed.");
+    } catch(Exception e) {
+      System.out.println("Connection NOT Closed.");
+    }//end try catch
+  }//end main
+}//end Class
