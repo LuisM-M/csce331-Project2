b6f65612328d7d8460b495301721ea5c64294f72
jianliu200 <jiangyuanliu2002@gmail.com>
2022-10-10 18:56:05 -0500

    added new things into the button section

--- gui.java
+++ gui.java
@@ -2,21 +2,65 @@ import javax.swing.*;
 public class gui{
     public gui(){
         JFrame system = new JFrame();
-        JButton button = new JButton("Sandwichs");
-        button.setBounds(300,0,100,50);
+        JButton button = new JButton("Chicken Sandwich");
+        button.setBounds(300,0,200,50);
         system.add(button);
 
-        JButton salad = new JButton("Salids");
-        salad.setBounds(400, 0, 100, 50);
+        JButton salad = new JButton("Grilled Chicken Sandwich");
+        salad.setBounds(500, 0, 200, 50);
         system.add(salad);
 
-        JButton drinks = new JButton("Drinks");
-        drinks.setBounds(500, 0, 100,50);
+        JButton cookie = new JButton("Cookies");
+        cookie.setBounds(700, 0, 100, 50);
+        system.add(cookie);
+
+        JButton drinks = new JButton("Fountain Drinks");
+        drinks.setBounds(300, 50, 200,50);
         system.add(drinks);
 
-        JButton dessert = new JButton("Desserts");
-        dessert.setBounds(600, 0, 100,50);
+        JButton dessert = new JButton("Browie");
+        dessert.setBounds(500, 50, 100,50);
         system.add(dessert);
+
+        JButton fries = new JButton("Fries");
+        fries.setBounds(600,50,100,50);
+        system.add(fries);
+
+        JButton nuggets = new JButton("nuggets");
+        nuggets.setBounds(700, 50, 100, 50);
+        system.add(nuggets);
+
+        JButton club = new JButton("Grilled Chicken Club");
+        club.setBounds(300, 100, 250, 50);
+        system.add(club);
+
+        JButton coolwrap = new JButton("Grilled Chicken Cool Wrap");
+        coolwrap.setBounds(550, 100, 250, 50);
+        system.add(coolwrap);
+
+        JButton spicy = new JButton("Spicy Chicken Sandwich");
+        spicy.setBounds(300, 150, 200, 50);
+        system.add(spicy);
+
+        JButton deluxe = new JButton("Chic-Fil-A Deluxe");
+        deluxe.setBounds(500, 150, 150, 50);
+        system.add(deluxe);
+
+        JButton sdeluxe = new JButton("Spicy Deluxe");
+        sdeluxe.setBounds(650, 150, 150, 50);
+        system.add(sdeluxe);
+
+        JButton sssalid = new JButton("Spicy Southwest Salad");
+        sssalid.setBounds(300, 200, 200, 50);
+        system.add(sssalid);
+
+        JButton cobbsalad = new JButton("Cobb Salad");
+        cobbsalad.setBounds(500, 200, 150, 50);
+        system.add(cobbsalad);
+
+        JButton marketsalid = new JButton("Market Salad");
+        marketsalid.setBounds(650, 200, 150, 50);
+        system.add(marketsalid);
 
         JButton manager = new JButton("Manger");
         manager.setBounds(0, 410, 100, 50);
