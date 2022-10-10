f677c241e0b4d3be05aa1cae47c673ab0b66f3b0
jianliu200 <jiangyuanliu2002@gmail.com>
2022-10-10 15:14:36 -0500

    starting out for the gui

--- gui.java
+++ gui.java
@@ -3,18 +3,26 @@ public class gui{
     public gui(){
         JFrame system = new JFrame();
         JButton button = new JButton("Sandwichs");
-        button.setBounds(100,0,100,30);
+        button.setBounds(300,0,100,50);
         system.add(button);
 
         JButton salad = new JButton("Salids");
-        salad.setBounds(200, 0, 100, 30);
+        salad.setBounds(400, 0, 100, 50);
         system.add(salad);
 
         JButton drinks = new JButton("Drinks");
-        drinks.setBounds(300, 0, 100, 30);
+        drinks.setBounds(500, 0, 100,50);
         system.add(drinks);
 
-        system.setSize(500,500);
+        JButton dessert = new JButton("Desserts");
+        dessert.setBounds(600, 0, 100,50);
+        system.add(dessert);
+
+        JButton manager = new JButton("Manger");
+        manager.setBounds(0, 410, 100, 50);
+        system.add(manager);
+
+        system.setSize(800,500);
         system.setLayout(null);
         system.setVisible(true);
 
